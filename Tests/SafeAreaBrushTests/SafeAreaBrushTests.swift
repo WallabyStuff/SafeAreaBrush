9804e7b2730593f8f91310170983629dd126e76c
SunneyG <avocado34.131@gmail.com>
2022-07-06 15:31:20 +0900

    Feat: Prepare for spm
    - 'Swift package init' executed

---
+++ Tests/SafeAreaBrushTests/SafeAreaBrushTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SafeAreaBrush
+
+final class SafeAreaBrushTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SafeAreaBrush().text, "Hello, World!")
+    }
+}
